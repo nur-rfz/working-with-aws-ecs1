eb85c45fcb8a786f395246cc902fbd3a2bc0f6d6
nur-rfz <56801102+nur-rfz@users.noreply.github.com>
2023-06-09 14:30:49 +0200

    Create variables.tf

---
+++ asg/variables.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  description = "AWS region to create resources in"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance type for launch template"
+  default     = "t3a.micro"
+}
+
+variable "nginx_port" {
+  type        = string
+  description = "Nginx port"
+  default     = 80
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "ECS cluster name"
+}
